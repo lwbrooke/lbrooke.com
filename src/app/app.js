4f1ca3efb4b12e58801500bc20f9983dae931300
Logan Brooke <logan.w.brooke@gmail.com>
2016-10-05 23:39:02 -0700

    4 space tabs

--- src/app/app.js
+++ src/app/app.js
@@ -1,25 +1,24 @@
 import angular from 'angular';
-
 import '../style/app.less';
 
-let app = () => {
-  return {
-    template: require('./app.html'),
-    controller: 'AppCtrl',
-    controllerAs: 'app'
-  }
+var app = function() {
+    return {
+        template: require('./app.html'),
+        controller: 'AppCtrl',
+        controllerAs: 'app'
+    }
 };
 
 class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
+    constructor() {
+        this.url = 'https://github.com/preboot/angular-webpack';
+    }
 }
 
 const MODULE_NAME = 'app';
 
 angular.module(MODULE_NAME, [])
-  .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+    .directive('app', app)
+    .controller('AppCtrl', AppCtrl);
 
-export default MODULE_NAME;
+// export default MODULE_NAME;
