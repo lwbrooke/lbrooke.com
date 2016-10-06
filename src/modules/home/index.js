a13c1e8d3b7b13f11bcad2b864f6817f406eee1d
Logan Brooke <logan.w.brooke@gmail.com>
2016-10-05 19:25:28 -0700

    remove more files

--- src/modules/home/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-import angular from 'angular';
-import PhoneListController from './phoneListController.js';
-console.log(PhoneListController)
-
-module.export = angular.module('lbrooke.com.home', [])
-    .controller('PhoneListController', PhoneListController)
-    .name;
